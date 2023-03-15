038bf99f2f1f616d13128acf141a37b8e3d54768
It22066466 <127720898+It22066466@users.noreply.github.com>
2023-03-15 17:01:17 +0000

    What did you change ?

--- .ccls-cache/@home@runner@tutorial-01-It22066466/Tute01.c
+++ .ccls-cache/@home@runner@tutorial-01-It22066466/Tute01.c
@@ -7,15 +7,17 @@
 int main() {
   int m1, m2,sum;
   float ave;
+//get user input 
   printf("Enter mark 1 : ");
   scanf("%d",&m1);
-  printf("Enter mark 1 : ");
-  scanf("%d",&m1);
-
+  printf("Enter mark 2 : ");
+  scanf("%d",&m2);
+//calculate sum
   sum = m1 + m2;
+//calculate average
   ave = sum /2.0;
-
-  printf("%.2f",ave);
+// display average 
+  printf("The average is %.2f",ave);
 
     
 
